7e1323d89487b03e283eb89e31347eafd661f558
curogihu <curogihu@users.noreply.github.com>
2024-08-17 10:19:38 +0900

    Fix something

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // BrowserRouter のインポートを追加
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import TopPage from './pages/TopPage';
