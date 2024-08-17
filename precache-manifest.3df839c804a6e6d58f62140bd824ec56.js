574cdeba35679fb10145c034b7b9d024fa6d0ed9
curogihu <curogihu@users.noreply.github.com>
2024-08-17 10:50:24 +0900

    Updates

---
+++ precache-manifest.3df839c804a6e6d58f62140bd824ec56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0f8a6f4ce9b9e786c937221adb70a5",
+    "url": "/hinatazaka_pen_light_color_quiz/index.html"
+  },
+  {
+    "revision": "096fce4f844c125bcd43",
+    "url": "/hinatazaka_pen_light_color_quiz/static/css/main.20d84cda.chunk.css"
+  },
+  {
+    "revision": "283a5338995ed67f62bc",
+    "url": "/hinatazaka_pen_light_color_quiz/static/js/2.3ee70faa.chunk.js"
+  },
+  {
+    "revision": "096fce4f844c125bcd43",
+    "url": "/hinatazaka_pen_light_color_quiz/static/js/main.7ec9c6c0.chunk.js"
+  },
+  {
+    "revision": "8cbc87e26a5227544e80",
+    "url": "/hinatazaka_pen_light_color_quiz/static/js/runtime~main.8944ef6b.js"
+  },
+  {
+    "revision": "ceccbad40946550bb16f1923c1463b81",
+    "url": "/hinatazaka_pen_light_color_quiz/static/media/heart_icon.ceccbad4.png"
+  }
+]);
